21493644f6b664c999276786428f717032187571
Kolawole Daniel <kolawolecoke@gmail.com>
2023-09-30 20:11:27 +0100

    litte style change mobile nav preparation

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const hamburger = document.querySelector(".hamburger");
+const mobileNav = document.querySelector(".mobile-nav");
 
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("open");
+  mobileNav.classList.toggle("display");
+  document.body.classList.toggle("overflow-hidden");
 });
